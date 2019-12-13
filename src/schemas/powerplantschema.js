342747525035c10e7825a3f6eef801f859d211f7
Viktor From <viktor.from91@gmail.com>
2019-12-13 13:26:18 +0100

    Update powerplantschema.js

--- src/schemas/powerplantschema.js
+++ src/schemas/powerplantschema.js
@@ -1,55 +1,20 @@
 const mongoose = require('mongoose');
 
-
 const PowerPlantSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    marketPrice: {
-        type: Number,
-        default: 0
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    batteryId: {
-        type: Number,
-        required: true
-    },
-    maxProduction: {
-        type: Number,
-        default: 30
-    },
-    minProduction: {
-        type: Number,
-        default: 0
-    },
-    currentProduction: {
-        type: Number,
-        default: 0
-    },
-    conversionRate: {
-        type: Number,
-        default: 0.7
-    },
-    acceleration: {
-        type: Number,
-        default: 1.5
-    },
-    statusMessage: {
-        type: String,
-        default: "FULLY OPERATIONAL"
-    },
-    startUp: {
-        type: Boolean,
-        default: true
-    },
-    powerOutage: {
-        type: Boolean,
-        default: false
-    }
+    timestamp: { type: Date, default: Date.now() },
+    name: String,
+    marketPrice: Object,
+    region: String,
+    battery: Object,
+    mathExpression: Object,
+    maxProduction: Number,
+    minProduction: Number,
+    currentProduction: Number,
+    conversionRate: Number,
+    acceleration: Number,
+    statusMessage: String,
+    startUp: Boolean,
+    powerOutage: Boolean
 });
 
 
