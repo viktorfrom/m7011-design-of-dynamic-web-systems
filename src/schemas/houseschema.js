0b562a5e69774a8c41043ea97b54a323ff9a7e03
Viktor From <viktor.from91@gmail.com>
2019-12-13 12:15:34 +0100

    Update houseschema.js

--- src/schemas/houseschema.js
+++ src/schemas/houseschema.js
@@ -2,46 +2,18 @@ const mongoose = require('mongoose');
 
 
 const HouseSchema = mongoose.Schema({
-    powerPlant: {
-        type: String,
-        required: true
-    },
-    marketPrice: {
-        type: Number,
-        default: 0
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: String,
-        required: true
-    },
-    batteryId: {
-        type: Number,
-        required: true
-    },
-    windTurbineId: {
-        type: Number,
-        required: true
-    },
-    maxHouseConsumption: {
-        type: Number,
-        default: 1.35
-    },
-    minHouseConsumption: {
-        type: Number,
-        default: 0
-    },
-    houseConsumption: {
-        type: Number,
-        default: 914/744
-    },
-    statusMessage: {
-        type: String,
-        default: "FULLY OPERATIONAL"
-    }
+        timestamp: { type: Date, default: Date.now() },
+        powerPlant: Object,
+        marketPrice: Object,
+        location: Object,
+        owner: Object,
+        battery: Object,
+        windTurbine: Object,
+        mathExpression: Object,
+        maxHouseConsumption: Number,
+        minConsumption: Number,
+        houseConsumption: Number,
+        statusMessage: String 
 });
 
 
