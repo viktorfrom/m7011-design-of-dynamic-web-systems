ab6f857e1111e6b88df461cee9d1a9ff4b31349e
Viktor From <viktor.from91@gmail.com>
2019-12-16 17:16:33 +0100

    Update powerplantschema.js

--- src/schemas/powerplantschema.js
+++ src/schemas/powerplantschema.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const PowerPlantSchema = mongoose.Schema({
     timestamp: { type: Date, default: Date.now() },
     name: String,
-    marketPrice: Object,
     region: String,
+    marketPrice: Object,
     battery: Object,
-    mathExpression: Object,
+    // mathExpression: Object,
     maxProduction: Number,
     minProduction: Number,
     currentProduction: Number,
-    conversionRate: Number,
-    acceleration: Number,
+    // conversionRate: Number,
+    // acceleration: Number,
     statusMessage: String,
-    startUp: Boolean,
-    powerOutage: Boolean
+    // startUp: Boolean,
+    // powerOutage: Boolean
 });
 
 
