dd3c7fa77e6e0bfaa402f15a4883012c55cc3b7b
Viktor From <viktor.from91@gmail.com>
2019-12-16 17:16:41 +0100

    Update regionschema.js

--- src/schemas/regionschema.js
+++ src/schemas/regionschema.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const RegionSchema = mongoose.Schema({
-        timestamp: { type: Date, default: Date.now() },
-        name: String,
-        mathExpression: Object,
-        maxWindSpeed: Number, 
-        minWindSpeed: Number,
-        currentWindSpeed: Number,
-        maxTemp: Number,
-        currentTemp: Number
+    timestamp: { type: Date, default: Date.now() },
+    name: String,
+    // mathExpression: Object,
+    maxWindSpeed: Number, 
+    minWindSpeed: Number,
+    currentWindSpeed: Number,
+    maxTemp: Number,
+    currentTemp: Number
 });
 
 
