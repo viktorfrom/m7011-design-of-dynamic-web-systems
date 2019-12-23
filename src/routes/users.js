e41c44524ba9f75b1e65599d42a1f4ea445bacfd
Viktor From <viktor.from91@gmail.com>
2019-12-23 18:16:40 +0100

    Add bcrypt to passwords

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-// const bcrypt = require('bcryptjs');
-// const passport = require('passport');
+const {
+  check,
+  validationResult
+} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 // Bring in User Model
 let User = require('../schemas/userschema.js');
 
 // Register Form
 router.get('/signup', function (req, res, next) {
-    res.render('signup', {
-        title: 'Green Lean Electrics'
-    });
+  res.render('signup', {
+    title: 'Green Lean Electrics'
+  });
 });
 
 
@@ -22,34 +24,56 @@ router.post('/signup', [
   check('email').isEmail(),
   check('email').notEmpty(),
   check('username').notEmpty(),
-  check('password').isLength({ min: 5 })
+  check('password').isLength({
+    min: 5
+  })
 ], (req, res) => {
-    console.log(req.body);
+  // console.log(req.body);
   // Finds the validation errors in this request and wraps them in an object with handy functions
   const errors = validationResult(req);
 
+  // console.log(errors);
   if (!errors.isEmpty()) {
-    res.redirect('/users/signup');
+    console.log(errors.errors);
+    // res.redirect('/users/signup');
   } else {
-    User.create({
+    let newUser = new User({
       name: req.body.name,
       email: req.body.email,
       username: req.body.username,
       password: req.body.password
-    }).then(user => res.json(user));
+    });
+
+    
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      bcrypt.hash(newUser.password, salt, function (err, hash) {
+        if (err) {
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function (err) {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            res.redirect('/users/signin');
+          }
+        });
+      });
+    });
   }
-
-
-
-
 });
 
 
 // Login Form
 router.get('/signin', function (req, res, next) {
-    res.render('signin', {
-        title: 'Green Lean Electrics'
-    });
+  res.render('signin', {
+    title: 'Green Lean Electrics'
+  });
 });;
 
 module.exports = router;
