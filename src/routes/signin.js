ca3093ed62dc66e387e581eca15c765804d67461
Viktor From <viktor.from91@gmail.com>
2019-12-23 16:31:03 +0100

    Delete register.ejs, signup.js, singin.js

--- src/routes/signin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('signin', { title: 'Green Lean Electrics' });
-});
-
-module.exports = router;
