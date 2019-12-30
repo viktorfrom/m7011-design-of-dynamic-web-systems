105e486db479cd9fcb47dc34137cfd7da7be5f2b
Viktor From <viktor.from91@gmail.com>
2019-12-30 14:19:38 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {
+  ensureAuthenticated
+} = require('../config/auth');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Green Lean Electrics' });
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Green Lean Electrics'
+  });
 });
 
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Green Lean Electrics' });
+router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
+  res.render('dashboard', {
+    title: 'Green Lean Electrics',
+    user: req.user
+  });
 });
 
 module.exports = router;
