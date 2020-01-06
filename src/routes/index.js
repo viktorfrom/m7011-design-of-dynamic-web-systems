4f4b6f8228e81c6f4a953ac6f4cc3e63a1826456
Viktor From <viktor.from91@gmail.com>
2020-01-06 13:54:03 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const {
   ensureAuthenticated
 } = require('../config/auth');
+const fetch = require('node-fetch');
 
+
+
+// const House = require('./api/houseapi.js');
 
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -13,10 +17,23 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
+  let test = getHouse(); 
+  // console.log("test " + test.owner);
+
   res.render('dashboard', {
     title: 'Green Lean Electrics',
+    house: req.test,
     user: req.user
   });
 });
+
+
+function getHouse() {
+  fetch('http://127.0.0.1:3000/api/house/viktor@test.com')
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
+
+}
 
 module.exports = router;
