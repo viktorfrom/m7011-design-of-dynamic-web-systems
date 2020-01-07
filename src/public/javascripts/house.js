997ed0072d3a446dd3d191ba5309daa29ff272e1
Viktor From <viktor.from91@gmail.com>
2020-01-07 10:21:44 +0100

    Add house.js

---
+++ src/public/javascripts/house.js
@@ -0,0 +1,27 @@
+function getHouse(userEmail) {
+  fetch('http://127.0.0.1:3000/api/house/' + userEmail)
+  .then(res => res.json())
+  .then(house => {
+    document.getElementById('owner').innerHTML = house.owner;
+    document.getElementById('region').innerHTML = house.region;
+    document.getElementById('maxHouseConsumption').innerHTML = house.maxHouseConsumption;
+    document.getElementById('houseConsumption').innerHTML = house.houseConsumption;
+
+    document.getElementById('batteryMaxCapacity').innerHTML = house.battery.maxCapacity;
+    document.getElementById('batteryCurrCapacity').innerHTML = house.battery.currentCapacity;
+
+    document.getElementById('windTurbineMaxPower').innerHTML = house.windTurbine.maxPower;
+    document.getElementById('windTurbineCurrPower').innerHTML = house.windTurbine.currentPower;
+    document.getElementById('windTurbineExcessPower').innerHTML = house.windTurbine.excessPower;
+    document.getElementById('statusMessage').innerHTML = house.statusMessage;
+
+    document.getElementById('powerPlant').innerHTML = house.powerPlant;
+
+    document.getElementById('marketPriceName').innerHTML = house.marketPrice.name;
+    document.getElementById('marketPriceElectricityPrice').innerHTML = house.marketPrice.electricityPrice;
+    document.getElementById('marketPriceCurrentPrice').innerHTML = house.marketPrice.marketPriceSchema.currentPrice;
+  })
+  .catch(err => console.log(err));
+}
+
+getHouse('viktor@test.com');
