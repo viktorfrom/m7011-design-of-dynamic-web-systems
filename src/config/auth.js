8d13aa8f2eb7931f03f648a83317cf999a259ca4
Viktor From <viktor.from91@gmail.com>
2020-01-08 09:23:13 +0100

    Update auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,15 +3,13 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next();
     }
-    //   req.flash('error_msg', 'Please log in to view that resource');
-    res.redirect('/users/signin');
+    const proto = req.protocol;
+    const host = req.get('host');
+    const uri = req.originalUrl;
+    const location = `${proto}://${host}${uri}`;
+
+    res.redirect('/users/signin?permission=true&redirect=' + encodeURI(location));
   },
-  // forwardAuthenticated: function(req, res, next) {
-  //   if (!req.isAuthenticated()) {
-  //     return next();
-  //   }
-  //   res.redirect('/dashboard');      
-  // }
 
   check_user(req, res, next) {
     if (req.user != 'admin') {
