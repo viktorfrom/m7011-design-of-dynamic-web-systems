4cf67e8332d52083268922ba7a1b77858fd1efb3
Viktor From <viktor.from91@gmail.com>
2020-01-16 14:31:02 +0100

    Update userapi.js variable naming

--- src/routes/api/userapi.js
+++ src/routes/api/userapi.js
@@ -9,19 +9,19 @@ const User = require('../../schemas/userschema');
 const Image = require('../../schemas/imageschema.js');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/uploads');
+    destination: function (req, file, callback) {
+        callback(null, './public/uploads');
     },
-    filename: function (req, file, cb) {
-        cb(null, (new Date().toISOString() + file.originalname).split(" ").join(""));
+    filename: function (req, file, callback) {
+        callback(null, (new Date().toISOString() + file.originalname).split(" ").join(""));
     }
 });
 
-const fileFilter = (req, file, cb) => {
+const fileFilter = (req, file, callback) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
+        callback(null, true);
     } else {
-        cb(null, false);
+        callback(null, false);
     }
 };
 
