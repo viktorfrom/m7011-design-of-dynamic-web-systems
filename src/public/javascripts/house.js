d80189a64b77a2ffd3944a6c1cca7317bf1390e3
Viktor From <viktor.from91@gmail.com>
2020-01-22 09:51:18 +0100

    Update fetch API

--- src/public/javascripts/house.js
+++ src/public/javascripts/house.js
@@ -2,3 +2,9 @@ function getHouses() {
     return fetch('http://127.0.0.1:3000/api/house/users/houses')
         .then(res => res.json())
 }
+
+
+function getHouseBatteryRatio() {
+    return fetch('http://127.0.0.1:3000/api/house/users/latestBatteryRatio')
+        .then(res => res.json())
+}
